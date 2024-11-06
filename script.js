cb47d071f3efdc2661cc2ebd58dc267cf3072ecf
Kat Tyushnyakova <avchimchik@gmail.com>
2024-11-06 14:56:43 +0500

    Код переделан и работает

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 'use strict'
 
-const getData = () => {
-   return fetch('db.json')
+const sendData = (object) => {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(object),
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        })
         .then(response => response.json())
-        .catch(error => {console.log(error)})
+        .then(result => console.log('Результат:', result))
+        .catch(error => console.log('Ошибка:', error))
 };
 
-const sendData = () => {
-    getData()
-        .then(object => {
-            fetch('https://jsonplaceholder.typicode.com/posts', {
-                method: 'POST',
-                body: JSON.stringify(object),
-                headers: {
-                    'Content-Type': 'application/json;charset=utf-8'
-                  }
-            })
-                .then(response => response.json()) 
-                .then(result => console.log('Результат:', result))
-                .catch(error => console.log('Ошибка:', error));
-        }) 
+const getData = () => {
+    return fetch('db.json')
+        .then(response => response.json())
+        .then(result => {
+            console.log('Результат:', result);
+            return result
+        })
+        .catch(error => {
+            console.log(error)
+        })
 };
 
-sendData();
+getData()
+    .then(result => {
+        sendData(result)
+    });
